342550565e3dcedcdc52950d6767c0595bf60afd
Sienna-Xanthe <59522096+Sienna-Xanthe@users.noreply.github.com>
2021-04-19 20:24:24 +0800

    上传文件练习
    上传

---
+++ 测试.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	/*int i;
+	char s[255];
+	std::cin >> s;
+	for (i = 0; i < 254; i++)
+	{
+		if (s[i] == '\0')
+		{
+			break;
+		}
+	}
+	for (; i < 254; i++)
+	{
+		s[i] = '\0';
+	}
+	s[4] = '\0';
+	for (i = 0; i < 255; i++)
+	{
+		std::cout << s[i];
+	}
+	std::cout << std::endl;
+	system("pause");*/
+	cout << "\\0";
+	return 0;
+}
